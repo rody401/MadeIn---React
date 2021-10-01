d986fe4f887a753495141e0d1adb8e42974cb10e
rody401 <rodygomes401@gmail.com>
2021-10-01 01:23:58 -0400

    updates

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const session = require('session');
 const app = express();
 const PORT = process.env.PORT || 5000;
 const productRoutes = require('./routes/productRoutes');
-const Product = require("./models/product")
+
 
 connectDB();
+
+app.use (session({ secret: 'secretkey' }));
 
 app.use(express.json())
 
